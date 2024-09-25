480c1f25cf24c9d82b433961275a5086ccd8628a
Guinn <eguinjonathan08@gmail.com>
2024-09-25 10:29:46 +0700

    added navbar and responsiveness to the navbar

--- src/app/components/Util/Navbar/index.tsx
+++ src/app/components/Util/Navbar/index.tsx
@@ -1,8 +1,12 @@
+import Link from 'next/link'
 import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className='bg-blue-500 flex justify-between p-4 md:flex-row flex-col'>
+      <Link href='/' className='font-bold text-white md:text-3xl text-2xl'>ANIMELIST</Link>
+      <input type="text" placeholder='Search Anime' className='p-2 md:mt-0 mt-4 md:w-1/3'/>
+    </nav>
   )
 }
 
