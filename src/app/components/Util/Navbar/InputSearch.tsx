7f5009193c70905ff3b157c0d44e64a984e00dd5
Guinn <eguinjonathan08@gmail.com>
2024-09-25 21:46:13 +0700

    added enter event handler so you can search when you hit enter key

--- src/app/components/Util/Navbar/InputSearch.tsx
+++ src/app/components/Util/Navbar/InputSearch.tsx
@@ -5,6 +5,13 @@ import { useRouter } from "next/navigation";
 
 const InputSearch = () => {
     const searchRef = useRef<HTMLInputElement>(null);    const router = useRouter();
+
+    const handleEnter = (event: React.KeyboardEvent<HTMLInputElement>) => {
+        if (event.key === "Enter") {
+            event.preventDefault();
+            router.push(`/search/${searchRef.current!.value}`);
+        }
+    }
     
     const handleSearch = (event: React.MouseEvent<HTMLButtonElement>) => {
         event.preventDefault();
@@ -17,7 +24,7 @@ const InputSearch = () => {
         type="text"
         placeholder="Search Anime"
         className="p-2 rounded-md md:w-80 sm:w-full w-full md:mt-0 sm:mt-3 mt-3 "
-        ref={searchRef}
+        ref={searchRef} onKeyDown={handleEnter}
       />
       <button className="absolute md:right-6 md:top-5 sm:right-14 sm:top-16 right-11 top-16" onClick={handleSearch}>
       <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#000000" viewBox="0 0 256 256"><path d="M229.66,218.34l-50.07-50.06a88.11,88.11,0,1,0-11.31,11.31l50.06,50.07a8,8,0,0,0,11.32-11.32ZM40,112a72,72,0,1,1,72,72A72.08,72.08,0,0,1,40,112Z"></path></svg>
--- src/app/search/[keyword]/page.tsx
+++ src/app/search/[keyword]/page.tsx
@@ -1,19 +1,25 @@
-import React from 'react'
+import React from "react";
+import AnimeList from "@/app/components/AnimeList";
 
 type Params = {
   params: {
-    keyword: string
-  }
-}
+    keyword: string;
+  };
+};
 
-const Page = ({ params }: Params) => {
-  console.log(params)
+const Page = async ({ params }: Params) => {
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_API_BASE_URL}/anime?q=${params.keyword}`
+  );
+  const data = await res.json();
   return (
     <>
-        <h1>Search Page</h1>
-        <p>ini adalah search page</p>
+      <div className="text-2xl font-bold m-5 flex justify-between">
+        <h1>Showing results for {params.keyword}</h1>
+      </div>
+      <AnimeList api={data} />
     </>
-  )
-}
+  );
+};
 
-export default Page
+export default Page;
