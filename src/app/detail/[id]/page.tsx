39813f0cddc18de9f4b5338175b0a9694b70ffbf
Guinn <eguinjonathan08@gmail.com>
2024-09-26 08:45:36 +0700

    added detail page (still working on it)

--- src/app/detail/[id]/page.tsx
+++ src/app/detail/[id]/page.tsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import React from "react";
+import getAnimeResponse from "@/app/libs/api";
+import Image from "next/image";
+import styles from "./Detail.module.css";
 
-const AnimeDetail = () => {
+type Params = {
+  params: {
+    id: string;
+  };
+};
+
+const Page = async ({ params }: Params) => {
+  const data = await getAnimeResponse(`anime/${params.id}`, ``);
+  console.log(data.data);
   return (
-    <div>AnimeDetail</div>
-  )
-}
+    <>
+      <div className="text-2xl font-bold md:ml-5 ml-4 mb-2 mt-8 flex justify-between">
+        {data.data.title}
+      </div>
+      <div className="text-lg font-bold m-5 mt-2 flex justify-between">
+        {data.data.title_japanese}
+      </div>
+      <div className="flex md:flex-row flex-col justify-between max-w-full">
+        <div className="md:m-0">
+          <Image
+            src={data.data.images.jpg.image_url}
+            alt="image"
+            width={350}
+            height={350}
+            className={`rounded-3xl md:ml-5 md:max-w-96 sm:max-w-64 max-w-64 ml-14 ${styles.img} `}
+          />
+        </div>
+        <div>
+          <p className="text-lg font-bold m-5 mt-2">Sypnosis</p>
+          <p className="text-lg font-bold m-5 mt-2 flex justify-between">
+            {data.data.synopsis}
+          </p>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default AnimeDetail
+export default Page;
