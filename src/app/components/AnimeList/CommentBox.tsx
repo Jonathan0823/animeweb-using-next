b970fb618edf8302df80c67f9d8ec01c271a7c50
Guinn <eguinjonathan08@gmail.com>
2024-09-28 09:58:23 +0700

    fixed error

--- src/app/components/AnimeList/CommentBox.tsx
+++ src/app/components/AnimeList/CommentBox.tsx
@@ -1,6 +1,14 @@
 import React from "react";
 import prisma from "@/app/libs/prisma";
 
+interface Comment{
+    id: number;
+    user_name: string;
+    comment: string;
+    mal_id: number;
+}
+
+
 const CommentBox = async ({ mal_id }: { mal_id: number }) => {
   const comment = await prisma.comment.findMany({
     where: {
@@ -9,7 +17,7 @@ const CommentBox = async ({ mal_id }: { mal_id: number }) => {
   });
   return (
     <div className="grid md:grid-cols-4 gap-4 grid-cols-2">
-      {comment.map((comment) => {
+      {comment.map((comment: Comment) => {
         return (
           <div key={comment.id} className="text-black bg-blue-500 px-3 py-2 rounded-2xl">
             <p className="font-bold text-black">{comment.user_name}</p>
