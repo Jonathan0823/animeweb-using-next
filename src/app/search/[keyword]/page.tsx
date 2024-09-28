c4e8cda24c2b8d111a3252e82bdc2da80b36ded2
Guinn <eguinjonathan08@gmail.com>
2024-09-28 20:46:42 +0700

    removed console.log since i don't need it anymore

--- src/app/search/[keyword]/page.tsx
+++ src/app/search/[keyword]/page.tsx
@@ -11,7 +11,6 @@ type Params = {
 const Page = async ({ params }: Params) => {
   const data = await getAnimeResponse("anime", `q=${params.keyword}&sfw=true`);
   const decodedKeyword = decodeURI(params.keyword);
-  console.log(data);
   return (
     <>
     {data.data.length === 0 ? <div className="text-2xl font-bold m-5 flex flex-row gap-4  ">No results found for <h1 className="text-blue-500">{decodedKeyword}</h1></div> : <div className="text-2xl font-bold m-5 flex justify-between">
