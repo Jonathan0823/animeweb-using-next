3958736de7e04a487d9c330370f6f28b1a9a424b
Guinn <eguinjonathan08@gmail.com>
2024-09-28 10:21:40 +0700

    added some changes to the comments ui

--- src/app/user/comments/page.tsx
+++ src/app/user/comments/page.tsx
@@ -6,7 +6,7 @@ const Comments = async () => {
     const user = await authUserSesion()
     const comments = await prisma.comment.findMany({
         where: {
-            user_email: user?.email
+            user_email: user?.email || ""
         }
     })
 
@@ -21,9 +21,9 @@ interface Comment{
   return (
     <>
     <h3 className="text-center font-bold text-3xl my-6">My Comments</h3>
-    <div className='grid md:grid-cols-4 grid-cols-2 gap-4'>
+    <div className='grid md:grid-cols-4 grid-cols-2 gap-4 mx-4'>
         {comments.map((comment: Comment, index:number) => (
-            <div key={index} className="text-black bg-blue-500 px-3 py-2 rounded-2xl">
+            <div key={index} className="text-black bg-blue-500 px-3 py-2 rounded-2xl mx-4l">
             <p className="font-bold text-white">{comment.title}</p>
             <hr />
             <p className="my-3">{comment.comment}</p>
